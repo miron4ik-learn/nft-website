770f62aec0a78baf4ecaefbf78e2ff618ca06e4d
miron4ik <miron4ik.life@yandex.ru>
2022-09-06 20:24:16 +0300

    Add scroll reveal animation

--- src/js/app.js
+++ src/js/app.js
@@ -1,3 +1,7 @@
+import ScrollReveal from 'scrollreveal'
+
+
+
 // Show Menu
 
 const navMenu  = document.getElementById('nav-menu'),
@@ -33,3 +37,40 @@ const scrollHeader = () => {
 }
 
 window.addEventListener('scroll', scrollHeader)
+
+
+
+// Scroll Reveal Animation
+
+const sr = ScrollReveal({
+  origin: 'bottom',
+  distance: '60px',
+  duration: 2500,
+})
+
+sr.reveal(`.home__images`, {
+  distance: '120px',
+  delay: 400,
+})
+
+sr.reveal(`.home__title`, {
+  delay: 1000,
+})
+
+sr.reveal(`.home__description`, {
+  delay: 1200,
+})
+
+sr.reveal(`.home__button`, {
+  delay: 1400,
+})
+
+sr.reveal(`.home__footer`, {
+  delay: 1600,
+})
+
+sr.reveal(`.home__data div`, {
+  origin: 'right',
+  interval: 100,
+  delay: 1800,
+})
